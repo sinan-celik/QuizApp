21d4e61684d60b3bab060c595b5d4a691766af26
sinan-celik <sinancelik87@gmail.com>
2020-02-29 23:06:32 +0300

    Add Project Files

---
+++ lib/src/models/states.dart
@@ -0,0 +1,11 @@
+import 'package:rebuilder/rebuilder.dart';
+
+class States {
+  final materialPage = RebuilderState();
+  final categories = RebuilderState();
+  final mainPage = RebuilderState();
+  final triviaPage = RebuilderState();
+  final summaryPage = RebuilderState();
+  final settingsPage = RebuilderState();
+  final tab = RebuilderState();
+}
