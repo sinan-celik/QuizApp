1e3809d5cf99885fd9e9ee9b452be93431228a3b
sinan-celik <sinancelik87@gmail.com>
2020-03-05 22:42:55 +0300

    Adding Banner Screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,60 @@ import 'src/homepage.dart';
 import 'src/datamodels/app_data.dart';
 import 'src/models/settings.dart';
 import 'src/models/theme.dart';
+import 'dart:async';
+import 'package:after_layout/after_layout.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new BannerScreen(),
+      routes: <String, WidgetBuilder>{
+        '/trivia': (BuildContext context) => new App(),
+      },
+    );
+  }
+}
+
+class BannerScreen extends StatefulWidget {
+  @override
+  BannerScreenState createState() => new BannerScreenState();
+}
+
+class BannerScreenState extends State<BannerScreen>
+    with AfterLayoutMixin<BannerScreen> {
+  void openApp(BuildContext context) {
+    var duration = const Duration(seconds: 5);
+    Timer(duration, () => getRoute(context));
+  }
+
+  void getRoute(BuildContext context) {
+    Navigator.of(context).pushNamed('/trivia');
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    // Calling the same function "after layout" to resolve the issue.
+    openApp(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return new Scaffold(body: new Container(color: Colors.red));
+    return new Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1547665979-bb809517610d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'),
+              fit: BoxFit.cover
+        ),
+      ),
+    );
+  }
+}
 
 class App extends StatelessWidget {
   final appModel = AppModel();
